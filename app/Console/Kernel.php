3258234e1391f33d9274e559644209f54bc8689a
ballack <rodrigue-dev>
2024-01-25 07:06:36 +0100

    init

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:app:load-fixture')->everyThreeMinutes();
+        $schedule->command('app:app:load-fixture')->everyMinute();
     }
 
     /**
